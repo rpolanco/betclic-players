e51e46817fcb03fccba85fdb823d144cf975606b
rolep <role.polanco@gmail.com>
2023-06-08 16:53:48 +0200

    Empty Ktpor project

---
+++ src/main/kotlin/players/betclic/com/Application.kt
@@ -0,0 +1,12 @@
+package players.betclic.com
+
+import io.ktor.server.application.*
+import players.betclic.com.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+}
