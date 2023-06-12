a608712713a335719b611eb46c2300e75c3f1d40
rolep <role.polanco@gmail.com>
2023-06-12 17:44:29 +0200

    Added docker / compose config.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # betclic-players
-Recruitment test
+
+Test application for Betclic recruitment process 
+
+# Requirements: 
+- Docker
+- Docker Compose
+
+# To run the project:
+
+```bash
+./gradlew buildFatJar
+docker build --no-cache -t betclic-players .
+docker-compose up -d
+```
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,13 @@ val koin_version: String by project
 plugins {
     kotlin("jvm") version "1.8.22"
     id("io.ktor.plugin") version "2.3.1"
+    application
 }
 
 group = "players.betclic.com"
 version = "0.0.1"
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+    mainClass.set("players.betclic.com.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
@@ -26,6 +27,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
+    implementation("io.ktor:ktor-server-cio:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 
@@ -34,7 +36,13 @@ dependencies {
     implementation("io.ktor:ktor-serialization-jackson:$ktor_version")
 
     // IoC Koin
-    implementation ("io.insert-koin:koin-ktor:$koin_version")
-    implementation ("io.insert-koin:koin-core:$koin_version")
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-core:$koin_version")
 
+}
+
+ktor {
+    fatJar {
+        archiveFileName.set("fat.jar")
+    }
 }
